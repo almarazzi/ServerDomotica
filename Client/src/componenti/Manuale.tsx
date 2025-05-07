62d2f831d89a65754630e3ea818d19a2bbd4e54a
Marazzi Alberto <almarazzi05@gmail.com>
2025-05-07 18:35:22 +0200

    Cambiato nome alla cartella del Client

---
+++ Client/src/componenti/Manuale.tsx
@@ -0,0 +1,49 @@
+import { Fragment, useEffect, useState,  } from "react";
+import { DayManuale } from "./DayManuale";
+
+
+interface Oragiorno{
+    readonly oraInizio: string;
+    readonly oraFine: string;
+    readonly day: number;
+}
+interface key{
+    key: string;
+    value:Oragiorno[];
+}
+export function Manuale(props:{mac:string}){  
+const [r, setr] = useState([] as Oragiorno[]);
+
+useEffect(() => {
+    let isactive= true;
+    const fetchData1 = async () => {
+        let data = await fetch("/api/RelaySwitch/GetWeekProgram" ,{method:'GET',headers: { 'Content-type': 'application/json; charl set=UTF-8' }});    
+        var res = await data.json() as key[];
+        res.map((u,_)=>{
+            if(u.key===props.mac)
+            {
+                setr(u.value);
+            }
+        })
+        if(isactive)
+        {
+            setTimeout(() =>{
+                fetchData1();
+            },500);
+        }
+    };
+    fetchData1();
+    return() => {
+        isactive=false;
+    };
+},[]);
+    return <Fragment>
+        {
+        [...Array(7)].map((_,i) => 
+            <div>
+            <DayManuale key={i}  dayOfWeek={i}  array={r} mac={props.mac} />
+            <hr/>
+            </div>
+            )}
+    </Fragment>    
+}
---
+++ Client/src/componenti/componenteEsp.tsx
@@ -0,0 +1,169 @@
+import {  useCallback, useEffect, useState} from "react";
+import { DebounceInput } from "react-debounce-input";
+import { Link } from "react-router-dom";
+
+
+interface Tutto {
+    state: boolean,
+    macricever: string
+}
+
+export function ComponenteEsp(props: { mac: string, ip: string, abilitazioe: boolean, nome: string }) {
+    const [nomeEsp, SetnomeEsp] = useState("");
+    const [mac, Setmac] = useState("");
+    const [focus, setFocus] = useState(false);
+    const [abilitazione, setAbilitazione] = useState(false);//setAbilitazione
+    const [Isofline, setIsoffline] = useState(true);//setAbilitazione
+    const [M, setM] = useState(false);
+    const [A, setA] = useState(false);
+
+    useEffect(() => {
+        const api = async () => {
+            await fetch("/apiEsp/NomeEsp", { body: JSON.stringify({ mac: mac, nomeEsp: nomeEsp }), method: "PUT", headers: { 'Content-type': 'application/json; charl set=UTF-8' } });
+        }
+        if (focus && mac !== "") {
+            api();
+        }
+    }, [nomeEsp, mac, focus]);
+
+    useEffect(() => {
+        const api = async () => {
+            await fetch("/apiEsp/abilitazione", { body: JSON.stringify({ abilitazione: abilitazione, mac: mac }), method: "PUT", headers: { 'Content-type': 'application/json; charl set=UTF-8' } });
+        }
+        if (abilitazione !== null && mac !== "") {
+            api();
+        }
+    }, [abilitazione, mac]);
+
+    const y = useCallback(async () => {
+
+        const inv = { stateProgrammManu: !M, macricever: props.mac };
+        await fetch("/api/RelaySwitch/stateProgrammManu", { method: "PUT", body: JSON.stringify(inv), headers: { 'Content-type': 'application/json; charl set=UTF-8' } });
+    }, [M]);
+    useEffect(() => {
+        let isactive = true;
+        const fetchData = async () => {
+            let data = await fetch("/api/RelaySwitch/GetProgrammManu", { method: 'GET', headers: { 'Content-type': 'application/json; charl set=UTF-8' } });
+            var res = await data.json() as Tutto[];
+            if (isactive) {
+                {
+                    res.map((u, _) => {
+                        if (u.macricever === props.mac) {
+                            setM(u.state)
+                        }
+                    })
+                }
+                setTimeout(() => {
+                    fetchData();
+
+                }, 500);
+            }
+        };
+        fetchData();
+        return () => {
+            isactive = false;
+        };
+    }, []);
+
+    const p1 = useCallback(async () => {
+        const inv = { stateProgrammAuto: !A, macricever: props.mac };
+        await fetch("/api/RelaySwitch/stateProgrammAuto", { method: "PUT", body: JSON.stringify(inv), headers: { 'Content-type': 'application/json; charl set=UTF-8' } })
+    }, [A]);
+
+    useEffect(() => {
+        let isactive = true;
+        const fetchData1 = async () => {
+            let data = await fetch("/api/RelaySwitch/GetProgrammAuto", { method: 'GET', headers: { 'Content-type': 'application/json; charl set=UTF-8' } });
+            let res = await data.json() as Tutto[];
+            if (isactive) {
+                {
+                    res.map((u, _) => {
+                        if (u.macricever === props.mac) {
+                            setA(u.state)
+                        }
+                    })
+                }
+                setTimeout(() => {
+                    fetchData1();
+                }, 500);
+            }
+        };
+        fetchData1();
+        return () => {
+            isactive = false;
+        };
+    }, []);
+
+    useEffect(() => {
+        let isactive = true;
+        const fetchData = async () => {
+            let data = await fetch("/apiEsp/StatoRelay", { method: 'GET', headers: { 'Content-type': 'application/json; charl set=UTF-8' } });
+            var res = await data.json() as [];
+            if (isactive) {
+                {
+                    res.map((u, _) => {
+                        if (u === props.mac) {
+                            setIsoffline(true);
+                        }else
+                        {
+                            setIsoffline(false);
+                        }
+                    })
+                    if (res.length === 0)
+                    {
+                    setIsoffline(false);
+                    }
+                }
+                setTimeout(() => {
+                    fetchData();
+
+                }, 500);
+            }
+        };
+        fetchData();
+        return () => {
+            isactive = false;
+        };
+    }, [Isofline]);
+
+    return (
+        <div className="ccccc" >
+            <DebounceInput
+                type="text"
+                minLength={4}
+                debounceTimeout={500}
+                className="form-control input_ESP"
+                id="input"
+                placeholder="Nome Esp"
+                onFocus={() => setFocus(true)}
+                onBlur={() => setFocus(false)}
+                value={(focus === false ? props.nome : nomeEsp)}//(focus === false ? u.value.nomeEspClient : nomeEsp)
+                onChange={(e) => {
+                    SetnomeEsp(e.target.value);
+                    Setmac(props.mac);
+                }} />
+            <div className="form-check input_check">
+                <input className="form-check-input" type="checkbox" checked={props.abilitazioe} onChange={(e) => { setAbilitazione(e.target.checked); Setmac(props.mac); }} id="abiliatazione" required />
+                <label form="abiliatazione"> Abilitazione {props.nome}</label>
+            </div>
+
+            <div className="ip">IP:{props.ip}</div>
+            <div className="mac">MAC:{props.mac}</div>
+            <div className="ip">Stato Relay:{(Isofline === true ? "Offline" : "Online")}</div>
+            <div className="componenteAutoManu">
+                <input className="form-check-input casellaAuto" type="checkbox" checked={A} onChange={p1} id="invalidCheck1" required />
+
+                <Link to={"/Manuale/" + props.mac} className="bottoneAuto dropdown-item">
+                    <button type="button" className="btn btn-outline-primary ">Automatico</button>
+                </Link>
+
+                <Link to={"/Automatico/" + props.mac} className="bottoneManu dropdown-item">
+                    <button type="button" className="btn btn-outline-primary ">Manuale</button>
+                </Link>
+
+                <input className="form-check-input casellaManu" type="checkbox" checked={M} onChange={y} id="invalidCheck" required />
+            </div>
+        </div>
+
+    );
+}
