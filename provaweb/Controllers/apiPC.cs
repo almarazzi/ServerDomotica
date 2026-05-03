ac86e1c28130194729cc06eb3e6f27ea5d33c293
almar <almar@PC_ALBY>
2026-05-03 09:31:17 +0200

    Refactor Spazio to use double with rounded values
    Changed SpazioLibero and SpazioTotale to double for better precision. Updated Spazio.Empty accordingly. Modified SpazioDig to cast values to double and round to two decimals for improved accuracy and readability.

--- provaweb/Controllers/apiPC.cs
+++ provaweb/Controllers/apiPC.cs
@@ -10,9 +10,9 @@ namespace provaweb
     {
         private readonly ILogger<Login> m_logger;
         private readonly DriveInfo m_driveInfo;
-        public record Spazio(string Nome, long SpazioLibero, long SpazioTotale)
+        public record Spazio(string Nome, double SpazioLibero, double SpazioTotale)
         {
-            public static readonly Spazio Empty = new("",0,0);
+            public static readonly Spazio Empty = new("",0.0,0.0);
         };
 
         public apiPC(ILogger<Login> logger,DriveInfo driveInfo)
@@ -26,7 +26,7 @@ namespace provaweb
         public async Task<IActionResult> SpazioDig()
         {
             
-            var g = Spazio.Empty with { Nome = m_driveInfo.Name, SpazioLibero = m_driveInfo.TotalFreeSpace/(1024*1024*1024), SpazioTotale = m_driveInfo.TotalSize/(1024*1024*1024) };
+            var g = Spazio.Empty with { Nome = m_driveInfo.Name, SpazioLibero =Math.Round((double)(m_driveInfo.TotalFreeSpace)/(1024*1024*1024),2), SpazioTotale = Math.Round((double)(m_driveInfo.TotalSize)/(1024*1024*1024),2)};
             return Ok(g);
         }
     }
