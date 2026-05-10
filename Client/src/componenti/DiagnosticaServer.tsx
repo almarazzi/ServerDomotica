7d313356ca5d32c538e84d3c197fcb52eaa14b49
almar <almar@PC_ALBY>
2026-05-10 17:00:26 +0200

    Format SpazioOccupato with two decimal places
    Updated the display of SpazioOccupato in DiagnosticaServer.tsx to use toFixed(2), ensuring the value is shown with two decimal places for improved readability.

--- Client/src/componenti/DiagnosticaServer.tsx
+++ Client/src/componenti/DiagnosticaServer.tsx
@@ -40,7 +40,7 @@ return ()=>{isActive = false}
             <div className="Diag fw-bolder" >Spazio sul disco PC server: Dsco Locale({Nome})</div>
             <div className="Diag fw-bolder" >Spazio Tot sul disco PC server: {SpazioTotale} gb</div>
             <div className="Diag fw-bolder" >Spazio Libero sul disco PC server: {SpazioLibero} gb</div>
-            <div className="Diag fw-bolder" >Spazio Occupato sul disco PC server: {SpazioOccupato} gb</div>
+            <div className="Diag fw-bolder" >Spazio Occupato sul disco PC server: {SpazioOccupato.toFixed(2)} gb</div>
             <div className="progress">
             <div className=" progress-bar bg-success" role="progressbar" style={{ width: tot.toFixed(2) + '%' }} aria-valuemin={0} aria-valuemax={100}></div>
             </div>
